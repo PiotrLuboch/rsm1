76e4fff43c1b15e0cd4d1a9c439ac91f498fa96d
Piotr Luboch <200552@student.pwr.edu.pl>
2016-06-01 17:55:59 +0200

    Added CSV files

--- src/com/rsm/Algorithms.java
+++ src/com/rsm/Algorithms.java
@@ -4,34 +4,55 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Random;
 
-public class Algorithms {
+public class Algorithms
+{
 	ArrayList<Person> persons;
 	ArrayList<Table> tables;
 	SatisfactionMatrix matrix;
 
-	public Algorithms(Facade facade) {
+	public Algorithms(Facade facade)
+	{
 		this.persons = facade.persons;
 		this.tables = facade.tables;
 		this.matrix = facade.satisfactionMatrix;
 	}
 
-	public double compute() {
-		// optimizePeopleSittingPlaces();
+	public double performGreedy()
+	{
+		unsitPersons();
+		sitPeopleUsingGreedyAlgorithm();
+		return goalFunction();
+	}
+
+	public double performRandom()
+	{
+		unsitPersons();
 		sitPeopleRandomly();
 		return goalFunction();
 	}
 
-	public void optimizePeopleSittingPlaces() {
+	private void unsitPersons()
+	{
+		persons.forEach(x -> x.isSitted = false);
+		tables.forEach(x-> x.persons.clear());
+	}
+
+	public void sitPeopleUsingGreedyAlgorithm()
+	{
 		Iterator<Table> it = tables.iterator();
 
-		while (it.hasNext()) {
+		while (it.hasNext())
+		{
 			Table t = it.next();
-			while (t.hasFreeSits()) {
-				if (t.isEmpty()) {
+			while (t.hasFreeSits())
+			{
+				if (t.isEmpty())
+				{
 					Pair<Person> p = matrix.findBestPairToSit(persons);
 					t.sitPerson(p.item1);
 					t.sitPerson(p.item2);
-				} else {
+				} else
+				{
 					Person p = matrix.findBestPersonToSit(persons);
 					t.sitPerson(p);
 				}
@@ -39,7 +60,8 @@ public class Algorithms {
 		}
 	}
 
-	public void sitPeopleRandomly() {
+	public void sitPeopleRandomly()
+	{
 		Iterator<Table> it = tables.iterator();
 
 		Random generator = new Random();
@@ -47,13 +69,16 @@ public class Algorithms {
 		int index;
 		ArrayList<Integer> tempPersons = new ArrayList<>();
 
-		for (int i = 0; i < persons.size(); ++i) {
+		for (int i = 0; i < persons.size(); ++i)
+		{
 			tempPersons.add(i);
 		}
 
-		while (it.hasNext()) {
+		while (it.hasNext())
+		{
 			Table t = it.next();
-			while (t.hasFreeSits()) {
+			while (t.hasFreeSits())
+			{
 				index = generator.nextInt(tempPersons.size());
 				personID = tempPersons.get(index);
 				t.sitPerson(persons.get(personID));
@@ -63,9 +88,11 @@ public class Algorithms {
 		}
 	}
 
-	public double goalFunction() {
+	public double goalFunction()
+	{
 		double d = 0;
-		for (Table t : tables) {
+		for (Table t : tables)
+		{
 			ArrayList<Integer> list = t.getSittingPeopleId();
 			for (Integer i : list)
 				for (Integer j : list)
--- src/com/rsm/mainClass.java
+++ src/com/rsm/mainClass.java
@@ -7,13 +7,18 @@ public class mainClass
 
 	public static void main(String[] args)
 	{
-		String fileName = "data/in_4.csv";
+		String fileName = "data/in_16.csv";
 		CSVFileReader fr = new CSVFileReader(fileName);
 		ArrayList<ArrayList<Double>> tab = fr.readFile(",");
 		
-		Facade f = new Facade(tab,4,1);
-		Algorithms greedyAlgorithm = new Algorithms(f);
-		double d = greedyAlgorithm.compute();
+		Facade f = new Facade(tab,4,4);
+		Algorithms algorithm = new Algorithms(f);
+		double d = algorithm.performGreedy();
+		for(Table t: f.tables)
+			System.out.println(t.toString());
+		System.out.println("Goal function value is: " + d);
+		
+		d = algorithm.performRandom();
 		for(Table t: f.tables)
 			System.out.println(t.toString());
 		System.out.println("Goal function value is: " + d);
